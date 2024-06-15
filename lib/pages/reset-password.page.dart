ad17f04552d28a340ca5b85ab5f3c45dfe619e23
Fabricio003 <fabriciooliveira.hub@outlook.com>
2024-06-15 01:24:09 -0300

    refactor: valide reset password with firebase

--- lib/pages/reset-password.page.dart
+++ lib/pages/reset-password.page.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'login.page.dart';
 
-class ResetPasswordPage extends StatelessWidget {
+class ResetPasswordPage extends StatefulWidget {
+  @override
+  _ResetPasswordPageState createState() => _ResetPasswordPageState();
+}
+
+class _ResetPasswordPageState extends State<ResetPasswordPage> {
   final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _newPasswordController = TextEditingController();
+  final _confirmNewPasswordController = TextEditingController();
+  bool _obscureNewPassword = true;
+  bool _obscureConfirmNewPassword = true;
 
   @override
   Widget build(BuildContext context) {
@@ -35,22 +48,14 @@ class ResetPasswordPage extends StatelessWidget {
                           Text(
                             "Esqueceu sua senha?",
                             style: TextStyle(
-                              fontSize: 32,
+                              fontSize: 25,
                               fontWeight: FontWeight.w500,
                             ),
                             textAlign: TextAlign.center,
                           ),
                           SizedBox(height: 30),
-                          Text(
-                            "Por favor, informe o E-mail associado a sua conta que enviaremos um link para o mesmo com as instruções para restauração de sua senha.",
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w400,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                          SizedBox(height: 30),
                           TextFormField(
+                            controller: _emailController,
                             keyboardType: TextInputType.emailAddress,
                             decoration: InputDecoration(
                               labelText: "E-mail",
@@ -64,6 +69,73 @@ class ResetPasswordPage extends StatelessWidget {
                             validator: (value) {
                               if (value == null || value.isEmpty) {
                                 return 'O campo E-mail é obrigatório';
+                              }
+                              return null;
+                            },
+                          ),
+                          SizedBox(height: 30),
+                          TextFormField(
+                            controller: _newPasswordController,
+                            obscureText: _obscureNewPassword,
+                            decoration: InputDecoration(
+                              labelText: "Nova senha",
+                              labelStyle: TextStyle(
+                                color: Colors.black38,
+                                fontWeight: FontWeight.w400,
+                                fontSize: 20,
+                              ),
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  _obscureNewPassword
+                                      ? Icons.visibility
+                                      : Icons.visibility_off,
+                                ),
+                                onPressed: () {
+                                  setState(() {
+                                    _obscureNewPassword = !_obscureNewPassword;
+                                  });
+                                },
+                              ),
+                            ),
+                            style: TextStyle(fontSize: 20),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'O campo Nova senha é obrigatório';
+                              }
+                              return null;
+                            },
+                          ),
+                          SizedBox(height: 30),
+                          TextFormField(
+                            controller: _confirmNewPasswordController,
+                            obscureText: _obscureConfirmNewPassword,
+                            decoration: InputDecoration(
+                              labelText: "Confirmar senha",
+                              labelStyle: TextStyle(
+                                color: Colors.black38,
+                                fontWeight: FontWeight.w400,
+                                fontSize: 20,
+                              ),
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  _obscureConfirmNewPassword
+                                      ? Icons.visibility
+                                      : Icons.visibility_off,
+                                ),
+                                onPressed: () {
+                                  setState(() {
+                                    _obscureConfirmNewPassword = !_obscureConfirmNewPassword;
+                                  });
+                                },
+                              ),
+                            ),
+                            style: TextStyle(fontSize: 20),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'O campo Confirmar senha é obrigatório';
+                              }
+                              if (value != _newPasswordController.text) {
+                                return 'As senhas não coincidem';
                               }
                               return null;
                             },
@@ -87,7 +159,7 @@ class ResetPasswordPage extends StatelessWidget {
                                 mainAxisAlignment: MainAxisAlignment.center,
                                 children: <Widget>[
                                   Text(
-                                    "Enviar",
+                                    "Salvar",
                                     style: TextStyle(
                                       fontWeight: FontWeight.bold,
                                       color: Colors.white,
@@ -97,9 +169,36 @@ class ResetPasswordPage extends StatelessWidget {
                                   ),
                                 ],
                               ),
-                              onPressed: () {
+                              onPressed: () async {
                                 if (_formKey.currentState!.validate()) {
-                                  // Ação para enviar email de recuperação
+                                  try {
+                                    await _resetPassword();
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(
+                                        content: Text('Nova senha registrada com sucesso!'),
+                                        backgroundColor: Colors.green,
+                                        shape: const RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
+                                        ),
+                                      ),
+                                    );
+                                    Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => LoginPage(),
+                                      ),
+                                    );
+                                  } catch (e) {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(
+                                        content: Text(e.toString()),
+                                        backgroundColor: Colors.red,
+                                        shape: const RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
+                                        ),
+                                      ),
+                                    );
+                                  }
                                 }
                               },
                             ),
@@ -131,5 +230,42 @@ class ResetPasswordPage extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  Future<void> _resetPassword() async {
+    final email = _emailController.text.trim();
+    final newPassword = _newPasswordController.text.trim();
+
+    final auth = FirebaseAuth.instance;
+    final ref = FirebaseDatabase.instance.ref().child('users');
+
+    final query = ref.orderByChild('email').equalTo(email).limitToFirst(1);
+    final snapshot = await query.get();
+
+    if (snapshot.exists) {
+      final userKey = snapshot.children.first.key;
+      final userData = snapshot.children.first.value as Map;
+
+      final currentPassword = userData['senha'];
+
+      final user = auth.currentUser;
+      if (user != null) {
+        final cred = EmailAuthProvider.credential(email: email, password: currentPassword);
+        try {
+          await user.reauthenticateWithCredential(cred);
+          await user.updatePassword(newPassword);
+          
+          await ref.child(userKey!).update({'senha': newPassword});
+          
+          await auth.signOut();
+        } catch (e) {
+          throw Exception('Falha na reautenticação. Verifique a senha atual e tente novamente.');
+        }
+      } else {
+        throw Exception('Usuário não encontrado.');
+      }
+    } else {
+      throw Exception('Usuário não encontrado.');
+    }
   }
 }
