edba7cb87567b4769db2a71fbaa21f1209b40154
Fabricio003 <fabriciooliveira.hub@outlook.com>
2024-06-15 01:27:03 -0300

    refactor: remove functions

--- lib/pages/config.page.dart
+++ lib/pages/config.page.dart
@@ -4,8 +4,8 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter_mobile_app/pages/login.page.dart';
 import 'package:flutter_mobile_app/services/auth.service.dart';
-import 'dart:io' show File; // Usado para outras plataformas que não são web
-import 'dart:typed_data'; // Usado para web
+import 'dart:io' show File;
+import 'dart:typed_data';
 import 'package:flutter/foundation.dart' show kIsWeb;
 
 class ConfigPage extends StatefulWidget {
@@ -14,13 +14,7 @@ class ConfigPage extends StatefulWidget {
 }
 
 class _ConfigPageState extends State<ConfigPage> {
-  final _formKey = GlobalKey<FormState>();
-  final TextEditingController _newPasswordController = TextEditingController();
-  final TextEditingController _currentPasswordController =
-      TextEditingController();
   final AuthService _authService = AuthService();
-  bool _newPasswordVisible = false;
-  bool _currentPasswordVisible = false;
   String? _imageUrl;
 
   @override
@@ -34,53 +28,6 @@ class _ConfigPageState extends State<ConfigPage> {
     setState(() {
       _imageUrl = imageUrl;
     });
-  }
-
-  @override
-  void dispose() {
-    _currentPasswordController.dispose();
-    _newPasswordController.dispose();
-    super.dispose();
-  }
-
-  void _submitForm() async {
-    if (_formKey.currentState?.validate() ?? false) {
-      try {
-        bool isReauthenticated =
-            await _authService.reauthenticate(_currentPasswordController.text);
-
-        if (isReauthenticated) {
-          await _authService.updatePassword(_newPasswordController.text);
-
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Text('Senha alterada com sucesso!'),
-              backgroundColor: Colors.green,
-              shape: const RoundedRectangleBorder(
-                  borderRadius:
-                      BorderRadius.vertical(top: Radius.circular(15))),
-            ),
-          );
-
-          _currentPasswordController.clear();
-          _newPasswordController.clear();
-        } else {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Text('Senha atual incorreta.'),
-              backgroundColor: Colors.red,
-            ),
-          );
-        }
-      } catch (e) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text('Erro ao atualizar a senha: $e'),
-            backgroundColor: Colors.red,
-          ),
-        );
-      }
-    }
   }
 
   Future<void> _pickImage() async {
@@ -141,7 +88,6 @@ class _ConfigPageState extends State<ConfigPage> {
         child: Column(
           children: <Widget>[
             _buildProfileHeader(),
-            _buildPasswordChangeSection(),
             _buildDeleteAccountButton(),
           ],
         ),
@@ -150,130 +96,31 @@ class _ConfigPageState extends State<ConfigPage> {
   }
 
   Widget _buildProfileHeader() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+    return Column(
       children: [
-        CircleAvatar(
-          backgroundColor: Colors.white, // Define um fundo branco
-          backgroundImage: _imageUrl != null && _imageUrl!.isNotEmpty
-              ? NetworkImage(_imageUrl!)
-              : null,
-          radius: 40,
-          child: _imageUrl == null || _imageUrl!.isEmpty
-              ? Icon(Icons.person, color: Colors.grey, size: 40)
-              : null,
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.deepOrange, width: 2),
+            borderRadius: BorderRadius.circular(50),
+          ),
+          child: CircleAvatar(
+            backgroundColor: Colors.white,
+            backgroundImage: _imageUrl != null && _imageUrl!.isNotEmpty
+                ? NetworkImage(_imageUrl!)
+                : null,
+            radius: 40,
+            child: _imageUrl == null || _imageUrl!.isEmpty
+                ? Icon(Icons.person, color: Colors.grey, size: 40)
+                : null,
+          ),
         ),
+        SizedBox(height: 10),
         IconButton(
           icon: Icon(Icons.camera_alt, color: Colors.deepOrange),
           onPressed: _pickImage,
         ),
       ],
     );
-  }
-
-  Widget _buildPasswordChangeSection() {
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(height: 30),
-          TextFormField(
-            controller: _currentPasswordController,
-            obscureText: !_currentPasswordVisible,
-            decoration: InputDecoration(
-              labelText: "Senha atual",
-              labelStyle: TextStyle(
-                color: Colors.black38,
-                fontWeight: FontWeight.w400,
-                fontSize: 20,
-              ),
-              suffixIcon: IconButton(
-                icon: Icon(_currentPasswordVisible
-                    ? Icons.visibility
-                    : Icons.visibility_off),
-                onPressed: () {
-                  setState(() {
-                    _currentPasswordVisible = !_currentPasswordVisible;
-                  });
-                },
-              ),
-            ),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Por favor, insira a senha atual';
-              }
-              return null;
-            },
-          ),
-          SizedBox(height: 30),
-          TextFormField(
-            controller: _newPasswordController,
-            obscureText: !_newPasswordVisible,
-            decoration: InputDecoration(
-              labelText: "Nova senha",
-              labelStyle: TextStyle(
-                color: Colors.black38,
-                fontWeight: FontWeight.w400,
-                fontSize: 20,
-              ),
-              suffixIcon: IconButton(
-                icon: Icon(_newPasswordVisible
-                    ? Icons.visibility
-                    : Icons.visibility_off),
-                onPressed: () {
-                  setState(() {
-                    _newPasswordVisible = !_newPasswordVisible;
-                  });
-                },
-              ),
-            ),
-            validator: _validateNewPassword,
-          ),
-          SizedBox(height: 60),
-          Container(
-            height: 50,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.grey.shade300,
-                width: 1.0,
-              ),
-              color: Colors.deepOrange,
-              borderRadius: BorderRadius.all(
-                Radius.circular(10),
-              ),
-            ),
-            child: TextButton(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    "Gravar",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontSize: 18,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              ),
-              onPressed: _submitForm,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  String? _validateNewPassword(String? value) {
-    if (value == null || value.isEmpty) {
-      return 'Por favor, insira a nova senha';
-    } else if (value.length < 6) {
-      return 'A senha deve ter pelo menos 6 caracteres';
-    }
-    return null;
   }
 
   Widget _buildDeleteAccountButton() {
